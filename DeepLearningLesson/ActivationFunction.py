6ae94e9ac234aeb81941890b0849b01f6900174d
atm004 <atm004@a-tm.co.jp>
2019-12-30 14:26:13 +0900

    some activation functions defined

--- DeepLearningLesson/ActivationFunction.py
+++ DeepLearningLesson/ActivationFunction.py
@@ -1,1 +1,18 @@
-import NumPy as np
+import numpy as np
+
+def step(x):
+    boolx = x.any() > 0
+    return boolx.astype(np.int)
+    # return np.where(np.array(x > 0), np.ones_like(x), np.zeros_like(x));
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    C = np.max(x)
+    exp_x = np.exp(x - C)
+    sum_exp = np.sum(exp_x)
+    return exp_x / sum_exp
